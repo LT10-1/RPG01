e92eca80788354dc3a123fcaaef689fdb7e0993d
Lam <lttlam1710@gmail.com>
2023-11-09 11:55:18 +0700

    AnimSetup

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,10 @@
+using JetBrains.Rider.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+}
